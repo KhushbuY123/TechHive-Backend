8d90efee6ad38e8e9257e4298f06a09b59993f6c
Khushbu Yadav <khushbuyad123@gmail.com>
2024-10-21 17:56:55 +0530

    multiple route handler

--- src/app.js
+++ src/app.js
@@ -6,15 +6,22 @@ const app=express();
 // handle the  route : route handler
 
 // This will only handle get calls to the root path
-app.use('/user',(req,res)=>{
-    res.send('he he he');
-});
-app.get('/user',(req,res)=>{
-    res.send({name:'John',age:30});
-});
+
+app.use('/user',[(req,res,next)=>{
+    console.log('First handler');
+    next();
+    res.send('uo uo uo');
+},(req,res,next)=>{
+    console.log('Second handler');
+    next();
+    res.send("no no no")
+},(req,res)=>{
+    console.log('Third handler');
+    res.send("hey hey hey")
+}]);
 
 app.post('/user',(req,res)=>{
-     console.log('Post request received');
+    console.log('Post request received');
     res.send('Post request received');
 });
 //this will match all the routes that have the word 'test' in them
