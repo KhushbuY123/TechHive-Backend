701a1bec740e698a488e689ae250ea6a99a3704d
Khushbu Yadav <khushbuyad123@gmail.com>
2024-10-21 14:09:47 +0530

    hanling routes

--- src/app.js
+++ src/app.js
@@ -3,10 +3,25 @@ const express = require('express');
 // create new application of express
 const app=express();
 
-// handle the  path
+// handle the  route : route handler
+
+// This will only handle get calls to the root path
+app.use('/user',(req,res)=>{
+    res.send('he he he');
+});
+app.get('/user',(req,res)=>{
+    res.send({name:'John',age:30});
+});
+
+app.post('/user',(req,res)=>{
+     console.log('Post request received');
+    res.send('Post request received');
+});
+//this will match all the routes that have the word 'test' in them
 app.use("/test",(req,res)=>{
     res.send('Hello World');
 })
+
 
 // listen to the root path
 app.listen(3000,()=>{
